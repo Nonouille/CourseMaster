f527c7fa93ba2cb8ebfb59c86115b28de6ab10c4
Nonouille <arnaud.py@edu.devinci.fr>
2023-12-12 18:25:27 +0100

    creation of ng pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from "@angular/common/http";
 import { LessonSelectComponent } from './lesson-select/lesson-select.component';
+import { CourseComponent } from './course/course.component';
+import { CourseEditComponent } from './course-edit/course-edit.component';
+import { StatistiquesComponent } from './statistiques/statistiques.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LessonSelectComponent
+    LessonSelectComponent,
+    CourseComponent,
+    CourseEditComponent,
+    StatistiquesComponent
   ],
   imports: [
     BrowserModule,
