00f98f2dbfa0d74aa8cc79e4785e07281d692559
Nonouille <arnaud.py@edu.devinci.fr>
2023-12-12 17:19:42 +0100

    http getting

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment} from "../environments/environment";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,13 @@ import { environment} from "../environments/environment";
 })
 export class AppComponent {
   title = 'frontend';
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     console.log(environment.production); // Logs false for development environment
+  }
+
+  onTestClick() {
+    this.httpClient.get('/api/learning-package').subscribe(res => {
+      console.log(res)
+    })
   }
 }
